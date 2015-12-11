b024e7e840d901d629e1c346464dba2ff88508a9
alex.khordos <Times12#>
2015-12-11 11:45:22 -0500

    new stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,46 @@
+var React = require('react'),
+    DOM = React.DOM, div = DOM.div, button = DOM.button, ul = DOM.ul, li = DOM.li
 
-var React = window.React = require('react'),
-    ReactDOM = require("react-dom"),
-    Timer = require("./ui/Timer"),
-    mountNode = document.getElementById("app");
+// This is just a simple example of a component that can be rendered on both
+// the server and browser
 
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText) {
-      return <li>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
-  }
-});
-var TodoApp = React.createClass({
+module.exports = React.createClass({
+
+  // We initialise its state by using the `props` that were passed in when it
+  // was first rendered. We also want the button to be disabled until the
+  // component has fully mounted on the DOM
   getInitialState: function() {
-    return {items: [], text: ''};
+    return {items: this.props.items, disabled: true}
   },
-  onChange: function(e) {
-    this.setState({text: e.target.value});
+
+  // Once the component has been mounted, we can enable the button
+  componentDidMount: function() {
+    this.setState({disabled: false})
   },
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
+
+  // Then we just update the state whenever its clicked by adding a new item to
+  // the list - but you could imagine this being updated with the results of
+  // AJAX calls, etc
+  handleClick: function() {
+    this.setState({
+      items: this.state.items.concat('Item ' + this.state.items.length)
+    })
   },
+
+  // For ease of illustration, we just use the React JS methods directly
+  // (no JSX compilation needed)
+  // Note that we allow the button to be disabled initially, and then enable it
+  // when everything has loaded
   render: function() {
-    return (
-      <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
-        <Timer />
-      </div>
-    );
-  }
-});
 
+    return div(null,
 
-ReactDOM.render(<TodoApp />, mountNode);
+        button({onClick: this.handleClick, disabled: this.state.disabled}, 'Add Item'),
 
+        ul({children: this.state.items.map(function(item) {
+          return li(null, item)
+        })})
+
+    )
+  },
+})
