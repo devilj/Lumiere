393db366297a1511f3d5437a8b5ad142515feebe
alex.khordos <Times12#>
2016-01-04 15:52:09 -0500

    a bunch of changes to make things work

--- server2.js
+++ server2.js
@@ -4,6 +4,7 @@ var browserify = require('browserify');
 var literalify = require('literalify');
 var express = require('express');
 var _ = require('lodash');
+var parse = require('./parse.js');
 
 var app = express();
 app.use(require('body-parser').json());
@@ -15,23 +16,36 @@ app.get('/data', function(req, res) {
 });
 
 app.get('/bundle.js', function(req, res) {
-    res.setHeader('Content-Type', 'text/javascript')
+    res.setHeader('Content-Type', 'text/javascript');
 
-    browserify()
-        .add('./browser.js')
-        .transform(literalify.configure({
-            'react': 'window.React',
-            'react-dom': 'window.ReactDOM'
-        }))
-        .bundle()
-        .pipe(res)
+    return browserify().add('./browser.js')
+    .add('./browser.js')
+    .transform(literalify.configure({
+        'react': 'window.React',
+        'react-dom': 'window.ReactDOM'
+    }))
+    .bundle()
+    .pipe(res);
+
 });
 
 
-var server = app.listen(8080, function () {
+var server = app.listen(8090, function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Lumiere backend listening at http://%s:%s', host, port);
+
 });
 
+app.get("/getdata", function(req, res) {
+
+    return parse.getTrainData(function(result) {
+        res.send(result);
+
+    });
+});
+
+
+
+//tar --exclude='node_modules' --exclude=".git" --exlcude="bower_components" -cvf Lumiere.tar Lumiere/
